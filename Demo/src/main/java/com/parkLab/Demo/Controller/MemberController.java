f4c570399e6570753d8f1360f6fc550b09782a5d
Park <qwe980813@naver.com>
2021-06-28 20:35:35 +0900

    spring #2

--- Demo/src/main/java/com/parkLab/Demo/Controller/MemberController.java
+++ Demo/src/main/java/com/parkLab/Demo/Controller/MemberController.java
@@ -8,27 +8,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Slf4j // 로그남기는 프레임워크
 @Controller
 public class MemberController {
 
     @Autowired
     private MemberRepository memberRepository;
+    private HttpSession session;
 
     public MemberController(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
 
     @RequestMapping("/")
-    public String login(){
+    public String login(HttpServletRequest request){
+
+        session = request.getSession();
+        if(session.getAttribute("id") != null){
+            return "welcome";
+        }
         return "login";
+
     }
 
     @PostMapping("/signIn")
-    public String signIn(String inputId, String inputPassword){
+    public String signIn(HttpServletRequest request, String inputId, String inputPassword){
         log.info("id : {}, pw : {}", inputId, inputPassword);
         Member member = this.memberRepository.findByIdAndPassword(inputId, inputPassword);
-        if (member != null){
+        if (member != null){  // 정보가 있다면
+            session = request.getSession();  // 세션을 생성한다.
+            session.setAttribute("id", inputId);  // id로 세션을 생성한다.
+            System.out.println("session = " + session.getAttribute("id"));
             return "loginSuccess";
         }
             return "loginFalse";
@@ -44,6 +57,17 @@ public class MemberController {
     public String create(Member member){
         System.out.println("member = " + member);
         this.memberRepository.save(member);
+        return "login";
+    }
+
+    @PostMapping("/logOut")
+    public String logOut(HttpServletRequest request, String id){
+
+        if (session != null) {
+            session.removeAttribute("id");
+            System.out.println("session is removed");
+            System.out.println("session = " + session.getAttribute("id"));
+        }
         return "login";
     }
 }
--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Spring과 JPA를 이용하여 Mysql DB에 회원 정보를 저장하는 회원�
 - ThymeLeaf
 - Lombok
 - Web
+
+
+
+1. 로그인 페이지 구현
+2. 회원가입 페이지 구현
+3. 로그인 기능 구현
+4. 세션 추가
